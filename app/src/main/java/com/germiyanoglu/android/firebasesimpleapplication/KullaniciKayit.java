fdb194e0b641d208eb9640b1461339e43ee2cbbc
Rapter1990 <sngermiyanoglu@hotmail.com>
2018-06-01 21:57:42 +0300

    commit changes

--- app/src/main/java/com/germiyanoglu/android/firebasesimpleapplication/KullaniciKayit.java
+++ app/src/main/java/com/germiyanoglu/android/firebasesimpleapplication/KullaniciKayit.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +16,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -132,6 +134,11 @@ public class KullaniciKayit extends AppCompatActivity {
 
                                                     Toast.makeText(getApplicationContext(),"Register Completed",Toast.LENGTH_SHORT).show();
                                                 }
+                                            }
+                                        }).addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.e("kullanicikayit : ", String.valueOf(e));
                                             }
                                         });
 
